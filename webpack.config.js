17eca119ca1cd348c47caebfb65bd26995c7e723
coed95 <codecoed95@gmail.com>
2024-07-09 19:40:46 +0200

    Update the config and fix knight.svg not showing up

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-
     entry: './src/index.js',
     plugins: [
         new HtmlWebpackPlugin({
@@ -11,13 +10,11 @@ module.exports = {
             title: 'template',
         }),
     ],
-
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
         clean: true,
     },
-
     module: {
         rules: [
             {
@@ -34,5 +31,4 @@ module.exports = {
             },
         ],
     },
-
 };
