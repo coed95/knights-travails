f4508b66999edfae6960af39787a19e39cb5d422
coed95 <codecoed95@gmail.com>
2024-07-09 19:41:06 +0200

    Add left and right click events to set up a start and end point for the travail

--- src/board.js
+++ src/board.js
@@ -1,6 +1,11 @@
+import knightImage from "./knight.svg";
+console.log("Knight URL: ", knightImage);
+
 export class Board {
     create(rows = 8, columns = 8) {
         const chessboard = document.getElementById("chessboard");
+        let knightPosition = null;
+        let markedSquare = null;
 
         for (let row = 0; row < rows; row++) {
             for (let column = 0; column < columns; column++) {
@@ -18,8 +23,33 @@ export class Board {
                 square.dataset.row = rows - row;
                 square.dataset.column = column + 1;
 
+                // left click to place a knight from where to start the travail
                 square.addEventListener("click", () => {
+                    if (knightPosition) {
+                        knightPosition.innerHTML = ''; // remove the knight from its previous position
+                    }
+
+                    const knight = document.createElement("div");
+                    knight.classList.add("knight");
+                    knight.style.backgroundImage = `url(${knightImage})`;
+                    square.appendChild(knight);
+                    knightPosition = square;
+
                     console.log(`[${square.dataset.column}, ${square.dataset.row}]`);
+                });
+
+                // right click to place an end point to where the knight should stop
+                square.addEventListener("contextmenu", (event) => {
+                    event.preventDefault(); // prevent the default context menu from appearing
+                    
+                    if (markedSquare) {
+                        markedSquare.classList.remove("marked");
+                    }
+
+                    square.classList.add("marked");
+                    markedSquare = square;
+
+                    console.log(`[${square.dataset.row}, ${square.dataset.column}]`);
                 });
 
                 chessboard.appendChild(square);
