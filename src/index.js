dc258aaac3efb3ae3c9cb861e1219181bdf81f45
coed95 <codecoed95@gmail.com>
2024-07-09 20:58:37 +0200

    Add the button events

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
 import "./styles.css";
 import { Board } from "./board.js";
+import { knightMoves } from "./knight.js";
 
 document.addEventListener("DOMContentLoaded", (event) => {
     const board = new Board();
-
     board.create();
+
+    const startButton = document.getElementById("startTravails");
+    startButton.addEventListener("click", () => {
+        if (board.positions.start && board.positions.end) {
+            const shortestPath = knightMoves(board.positions.start, board.positions.end);
+            console.log("Shortest path:", shortestPath);
+
+            // implement logic to highlight shortest path on chessboard if needed
+        }
+        else {
+            alert("Please set both start and end positions.");
+        }
+    });
+
+    const clearButton = document.getElementById("clearButton");
+    clearButton.addEventListener("click", () => {
+        board.clear();
+    });
 });
